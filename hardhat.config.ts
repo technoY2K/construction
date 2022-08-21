a25c48f222caf5f741f1764814d07cb2222cd225
technoY2K <cloud@kevia.me>
2022-08-21 16:23:07 -0700

    chore: save point after hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,13 @@ import { type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
     solidity: "0.8.9",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+    },
 };
 
 export default config;
