9bd2f77958a5416167c7a89280491bd9eafac66d
technoY2K <cloud@kevia.me>
2022-08-21 20:03:01 -0700

    feat: create a hook for wallet specific code

---
+++ app/hooks/useWalletConnect.ts
@@ -0,0 +1,4 @@
+import { useEffect } from "react";
+import { ethers } from "ethers";
+
+export default function useWalletConnect() {}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,7 @@
-import { useEffect } from "react";
-import { ethers } from "ethers";
-
 export default function Index() {
-    const clickHandler = () => {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-        provider
-            .send("eth_requestAccounts", [])
-            .then((accounts) => {
-                console.log(accounts);
-            })
-            .catch((e) => console.log(e));
-    };
-
     return (
         <div>
-            <button onClick={clickHandler}>Connect</button>
+            <button>Connect</button>
         </div>
     );
 }
