5c148729f3bffa600aa9cf3a3463278ff0dcddc9
technoY2K <cloud@kevia.me>
2022-08-21 20:34:12 -0700

    feat: connecting with metamask

--- app/hooks/useWalletConnect.ts
+++ app/hooks/useWalletConnect.ts
@@ -1,4 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-export default function useWalletConnect() {}
+export default function useWalletConnect() {
+    const [balance, setBalance] = useState<string | undefined>();
+    const [account, setAccount] = useState<string | undefined>();
+    const [detectMetaMask, setDetectMetaMask] = useState(true);
+
+    useEffect(() => {
+        setDetectMetaMask(!!window.ethereum);
+    }, []);
+
+    useEffect(() => {
+        if (!account || !ethers.utils.isAddress(account)) {
+            return;
+        }
+
+        if (!window.ethereum) {
+            return;
+        }
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        provider
+            .getBalance(account)
+            .then((balance) => {
+                setBalance(ethers.utils.formatEther(balance));
+            })
+            .catch((err) => console.error(err));
+    }, [account]);
+
+    const connect = () => {
+        if (!window.ethereum) {
+            return;
+        }
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        provider
+            .send("eth_requestAccounts", [])
+            .then((accounts) => {
+                if (accounts.length > 0) {
+                    setAccount(accounts[0]);
+                }
+            })
+            .catch((err) => console.error("Error connection wallet"));
+    };
+
+    const disconnect = () => {
+        setBalance(undefined);
+        setAccount(undefined);
+    };
+
+    return { account, balance, connect, disconnect, detectMetaMask };
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,17 @@
+import useWalletConnect from "~/hooks/useWalletConnect";
+
 export default function Index() {
+    const { account, balance, connect, disconnect } = useWalletConnect();
+
+    const clickHandler = account ? disconnect : connect;
+
     return (
         <div>
-            <button>Connect</button>
+            <p>{`Account: ${account ? account : ""}`}</p>
+            <p>{`Balance: ${balance ? balance : ""}`}</p>
+            <button onClick={clickHandler} style={{ cursor: "pointer" }}>
+                {account ? "Disconnect" : "Connect"}
+            </button>
         </div>
     );
 }
