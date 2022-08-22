29d6c9307ad4dfabd6eef4142323812799ea2afe
technoY2K <cloud@kevia.me>
2022-08-21 17:40:27 -0700

    chore: add hardhat files to .gitignore

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,9 +2,20 @@ import { useEffect } from "react";
 import { ethers } from "ethers";
 
 export default function Index() {
+    const clickHandler = () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        provider
+            .send("eth_requestAccounts", [])
+            .then((accounts) => {
+                console.log(accounts);
+            })
+            .catch((e) => console.log(e));
+    };
+
     return (
         <div>
-            <button></button>
+            <button onClick={clickHandler}>Connect</button>
         </div>
     );
 }
