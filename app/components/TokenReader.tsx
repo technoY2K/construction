6384d4459fe171dac0556ba638063c4485be09ff
technoY2K <cloud@kevia.me>
2022-08-27 12:09:08 -0700

    feat: read token balance from contract

--- app/components/TokenReader.tsx
+++ app/components/TokenReader.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
+import abi from "abi/contracts/FakeToken.sol/FakeToken.json";
+import { ethers } from "ethers";
+
 interface Props {
     contractAddress: string;
     currentAccount: string | undefined;
 }
 
 export default function TokenReader(props: Props) {
+    const [totalSupply, setTotalSupply] = useState<string>("0");
+
+    useEffect(() => {
+        if (!window.ethereum) {
+            return;
+        }
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const erc20 = new ethers.Contract(props.contractAddress, abi, provider);
+
+        erc20
+            .totalSupply()
+            .then((total: string) => {
+                setTotalSupply(ethers.utils.formatEther(total));
+            })
+            .catch("error", console.error);
+    }, [props.contractAddress]);
+
     return (
         <div>
             <p>{`ERC20 Contract: ${props.contractAddress}`}</p>
-            <p>{`FakeToken balance: `}</p>
+            <p>{`FakeToken balance: ${totalSupply}`}</p>
         </div>
     );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import useWalletConnect from "~/hooks/useWalletConnect";
+import TokenReader from "~/components/TokenReader";
 
 export default function Index() {
     const { account, balance, connect, disconnect } = useWalletConnect();
@@ -12,6 +13,12 @@ export default function Index() {
             <button onClick={clickHandler} style={{ cursor: "pointer" }}>
                 {account ? "Disconnect" : "Connect"}
             </button>
+            <div>
+                <TokenReader
+                    contractAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3"
+                    currentAccount={account}
+                />
+            </div>
         </div>
     );
 }
