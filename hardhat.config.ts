cee9bc02a8c93fe71e6e59cd02d39fc561d2feed
technoY2K <cloud@kevia.me>
2022-08-27 11:59:11 -0700

    feat: add a tool to export abi for solidity contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("hardhat-abi-exporter");
 
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
