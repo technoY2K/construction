637d6a0a87087b181876ec75e9f259c0ed25ed38
technoY2K <cloud@kevia.me>
2022-09-04 21:23:55 -0700

    refactor: add crossOrigin property to link descriptor

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,7 @@ export const links: LinksFunction = () => [
     {
         rel: "preconnect",
         href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
     },
     {
         rel: "stylesheet",
